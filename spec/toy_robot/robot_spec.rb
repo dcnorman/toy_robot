32dc4c16c8dbb65fa1f8e053383bbce4be487476
Daniel Norman <dcnorman@users.noreply.github.com>
2017-09-28 14:06:24 +0700

    Allow a Robot to be moved

--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -2,12 +2,16 @@ require "spec_helper"
 
 module ToyRobot
   class Location
+    NORTH = :north
+
     attr_reader :x
     attr_reader :y
+    attr_reader :facing
 
-    def initialize(x: 0, y: 0)
-      @x = x
-      @y = y
+    def initialize(x: 0, y: 0, facing: NORTH)
+      @x      = x
+      @y      = y
+      @facing = facing
     end
 
     # @note There is no real need for a Table object given the current
@@ -15,6 +19,14 @@ module ToyRobot
     #   table, no obstacles, etc. so this should suffice.
     def within_table_bounds?
       (0..5).cover?(x) && (0..5).cover?(y)
+    end
+
+    def next
+      Location.new(x: x, y: y + 1, facing: facing)
+    end
+
+    def ==(other_location)
+      x == other_location.x && y == other_location.y && facing == other_location.facing
     end
   end
 
@@ -25,6 +37,10 @@ module ToyRobot
       if new_location.within_table_bounds?
         @location = new_location
       end
+    end
+
+    def move
+      place(location.next)
     end
   end
 
@@ -42,6 +58,26 @@ module ToyRobot
             let(:location) { Location.new }
 
             it { is_expected.to eq(location) }
+
+            context "facing NORTH" do
+              context "after being moved with #move once" do
+                before { robot.move }
+
+                it { is_expected.to eq(Location.new(y: 1)) }
+              end
+
+              context "after being moved with #move twice" do
+                before { 2.times { robot.move } }
+
+                it { is_expected.to eq(Location.new(y: 2)) }
+              end
+
+              context "after being moved with #move 10 times" do
+                before { 10.times { robot.move } }
+
+                it { is_expected.to eq(Location.new(y: 5)) }
+              end
+            end
           end
 
           context "out of table bounds" do
